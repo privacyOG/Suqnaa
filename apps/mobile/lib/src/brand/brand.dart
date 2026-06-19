9cfa810ae674c9251ebc533ee8136a4961d55ba3
privacyOG <kriptiik@proton.me>
2026-06-20 00:02:30 +1000

    Add mobile brand tokens

---
+++ apps/mobile/lib/src/brand/brand.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SuqnaaBrand {
+  static const name = 'Suqnaa';
+  static const arabicName = 'سوقنا';
+  static const tagline = 'Discover. Connect. Trust.';
+
+  static const blue = Color(0xFF0B46D8);
+  static const teal = Color(0xFF18B9C5);
+  static const coral = Color(0xFFFF6958);
+  static const gold = Color(0xFFD9A441);
+  static const ivory = Color(0xFFFFFAF0);
+}
