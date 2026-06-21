9faae98f8b1063594ec5a0c37a970e521b9293af
privacyOG <kriptiik@proton.me>
2026-06-21 20:17:32 +1000

    Use scoped mobile app root

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,33 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:suqnaa/l10n/app_localizations.dart';
-import 'src/features/home/home_screen.dart';
+import 'package:flutter/widgets.dart';
+import 'src/app_root.dart';
 
 void main() {
-  runApp(const SuqnaaApp());
-}
-
-class SuqnaaApp extends StatelessWidget {
-  const SuqnaaApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Suqnaa',
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0B46D8)),
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFFFFFAF0),
-      ),
-      home: const HomeScreen(),
-    );
-  }
+  runApp(const SuqnaaRoot());
 }
