74c9b2da1d09302bd099a7335ab638b25be85b84
privacyOG <kriptiik@proton.me>
2026-06-22 01:46:40 +1000

    Remove unused challenge provider singleton

--- apps/api/src/security/challenge-provider.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '../config/env.js';
-import { createChallengeVerifier } from './challenge-verifier.js';
-
-export const challengeVerifier = createChallengeVerifier({
-  provider: env.CHALLENGE_PROVIDER,
-  secretKey: env.TURNSTILE_SECRET_KEY,
-  expectedHostname: env.TURNSTILE_EXPECTED_HOSTNAME,
-  timeoutMs: env.TURNSTILE_TIMEOUT_MS
-});
