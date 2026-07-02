0117d74a2297b7134dbb63db11c2e91d424bea21
privacyOG <kriptiik@proton.me>
2026-07-02 22:51:24 +1000

    Extend config test

--- apps/api/src/config/request-size.test.ts
+++ apps/api/src/config/request-size.test.ts
@@ -1,4 +1,5 @@
 import assert from 'node:assert/strict';
+import { resolveApiErrorResponse } from './http-error.js';
 import { resolveApiRequestSizeBytes } from './request-size.js';
 
 assert.equal(resolveApiRequestSizeBytes({}), 7340032);
@@ -7,3 +8,7 @@ assert.equal(resolveApiRequestSizeBytes({ value: '8388608' }), 8388608);
 assert.throws(() => resolveApiRequestSizeBytes({ value: '1048575' }), /1048576/);
 assert.throws(() => resolveApiRequestSizeBytes({ value: '8388609' }), /8388608/);
 assert.throws(() => resolveApiRequestSizeBytes({ value: 'invalid' }), /integer/);
+
+assert.equal(resolveApiErrorResponse({ name: 'ZodError' })?.statusCode, 400);
+assert.equal(resolveApiErrorResponse({ statusCode: 413 })?.statusCode, 413);
+assert.equal(resolveApiErrorResponse({ statusCode: 500 }), null);
